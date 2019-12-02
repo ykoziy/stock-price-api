aa0c13977d2c72516b5588418e912673788d0f6c
Yuriy Koziy <yukoziy@gmail.com>
2019-12-01 21:49:46 -0500

    Added failing functional test for 1 stock

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,37 +14,37 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
+
     suite('GET /api/stock-prices => stockData object', function() {
-      
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.property(res.body.stockData, "stock");
+          assert.property(res.body.stockData, "price");
+          assert.property(res.body.stockData, "likes");
+          assert.equal(res.body.stockData.stock, "GOOG");
           done();
         });
       });
-      
+
       test('1 stock with like', function(done) {
-        
+
       });
-      
+
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+
       });
-      
+
       test('2 stocks', function(done) {
-        
+
       });
-      
+
       test('2 stocks with like', function(done) {
-        
+
       });
-      
+
     });
 
 });
