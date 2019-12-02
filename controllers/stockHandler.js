5c0010e64cc51c00c028d16e83f3f8d08bf1d0e9
Yuriy Koziy <yukoziy@gmail.com>
2019-12-02 01:32:40 -0500

    Implemented controller functions, passes fucntional test for 1 stock.

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -1,5 +1,33 @@
-function StockHandler() {
+const fetch = require("node-fetch");
+const Like = require('../models/Like');
 
+async function getData(url) {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return {error: 'external source error'}
+    }
 }
 
-module.exports = StockHandler;
+async function getSingleStock(stockSymbol) {
+    const symbol = stockSymbol.toUpperCase();
+    const apiUrl = `https://repeated-alpaca.glitch.me/v1/stock/${symbol}/quote`;
+    const likes = await Like.getLikes(symbol);
+    const stockData = await getData(apiUrl);
+    return {stock: stockData.symbol, price: stockData.close, likes: likes};
+}
+
+async function getStocks(req, res, next) {
+  const stockSymbol = req.query.stock;
+  const stockData = await getSingleStock(stockSymbol);
+  if (stockData.error) {
+    res.json(stockData);
+  }
+  res.json({stockData: stockData});
+}
+
+module.exports = {
+  getStocks
+}
