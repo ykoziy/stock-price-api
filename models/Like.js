aee3c39bb51d350977d7e7f91fde21abd409d837
Yuriy Koziy <yukoziy@gmail.com>
2019-12-02 01:30:50 -0500

    Added static fucntion to Like model, removed default like count.

--- models/Like.js
+++ models/Like.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let LikeSchema = new Schema({
   stock: {type: String, required: true},
-  likes: {type: Number, required: true, default: 0},
+  likes: {type: Number, required: true},
   ips: [String]
 });
+
+LikeSchema.statics.getLikes = async function(symbol) {
+  const data = await this.find({stock: symbol});
+  return !data ? data.likes : 0;
+};
 
 module.exports = mongoose.model('Like', LikeSchema);
