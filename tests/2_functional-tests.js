0b2369e947a662d2007f2b356648d7af0cc8c5c2
Yuriy Koziy <yukoziy@gmail.com>
2019-12-02 23:48:29 -0500

    Added functional test for 1 stock and with like again. Code already passes this test.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -48,7 +48,18 @@ suite('Functional Tests', function() {
       });
 
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-
+        chai.request(server)
+         .get('/api/stock-prices')
+         .query({stock: 'goog', like: true})
+         .end(function(err, res){
+           assert.equal(res.status, 200);
+           assert.property(res.body.stockData, "stock");
+           assert.property(res.body.stockData, "price");
+           assert.property(res.body.stockData, "likes");
+           assert.equal(res.body.stockData.stock, "GOOG");
+           assert.equal(res.body.stockData.likes, 1);
+           done();
+         });
       });
 
       test('2 stocks', function(done) {
